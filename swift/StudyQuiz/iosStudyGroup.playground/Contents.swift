39ad511f396e0a668248e7e58e0b0f4cfca94af4
frozenxnow <frozenxnow@naver.com>
2021-07-30 20:03:08 +0900

    2021-07-30

--- swift/StudyQuiz/iosStudyGroup.playground/Contents.swift
+++ swift/StudyQuiz/iosStudyGroup.playground/Contents.swift
@@ -95,24 +95,89 @@ import Foundation
 //intermediate.nextCourse = advanced
 //department.courses = [intro, intermediate, advanced]
 
-class Country {
-    let name: String
-    var capitalCity: City!
-    init(name: String, capitalName: String) {
-        self.name = name
-        self.capitalCity = City(name: capitalName, country: self)
+//class Country {
+//    let name: String
+//    var capitalCity: City!
+//    init(name: String, capitalName: String) {
+//        self.name = name
+//        self.capitalCity = City(name: capitalName, country: self)
+//    }
+//}
+//
+//class City {
+//    let name: String
+//    unowned let country: Country
+//    init(name: String, country: Country) {
+//        self.name = name
+//        self.country = country
+//    }
+//}
+//
+//var country = Country(name: "Canada", capitalName: "Ottawa")
+//print("\(country.name)'s capital city is called \(country.capitalCity.name)")
+
+
+struct Resolution {
+    var width = 0
+    var height = 0
+}
+
+class VideoMode {
+    var resolution = Resolution()
+    var interlaced = false
+    var frameRate = 0.0
+    var name: String?
+}
+
+let someResolution = Resolution()
+let someVideoMode = VideoMode()
+
+print("The width of someResolution is \(someResolution.width)")
+print("The width of someVideoMode is \(someVideoMode.resolution.width)")
+
+someVideoMode.resolution.width = 1280
+print("The width of someVideoMode is \(someVideoMode.resolution.width)")
+
+let hd = Resolution(width: 1920, height: 1080)
+var cinema = hd
+
+cinema.width = 2048
+print("cinema is now \(cinema.width) pixels wide")
+print("hd is still \(hd.width) pixels wide")
+
+
+
+enum CompassPoint {
+    case north, south, east, west
+    mutating func turnNorth() {
+        self = .north
     }
 }
 
-class City {
-    let name: String
-    unowned let country: Country
-    init(name: String, country: Country) {
-        self.name = name
-        self.country = country
-    }
+var currentDirection = CompassPoint.west
+let rememberedDirection = currentDirection
+currentDirection.turnNorth()
+
+print("The current direction is \(currentDirection)")
+print("The remembered direction is \(rememberedDirection)")
+
+
+
+let tenEighty = VideoMode()
+tenEighty.resolution = hd
+tenEighty.interlaced = true
+tenEighty.name = "1080i"
+tenEighty.frameRate = 25.0
+
+let alsoTenEighty = tenEighty
+alsoTenEighty.frameRate = 30.0
+
+
+print("The frameRate property of tenEighty is now \(tenEighty.frameRate)")
+
+
+if tenEighty === alsoTenEighty {
+    print("tenEighty and alsoTenEighty refer to the same Video instance.")
 }
 
-var country = Country(name: "Canada", capitalName: "Ottawa")
-print("\(country.name)'s capital city is called \(country.capitalCity.name)")
 
