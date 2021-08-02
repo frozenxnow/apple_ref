aac9aa372214a6db5f54ea8dd86d9b8262d72a80
frozenxnow <frozenxnow@naver.com>
2021-08-02 18:58:45 +0900

    2021-08-02

--- swift/StudyQuiz/iosStudyGroup.playground/Contents.swift
+++ swift/StudyQuiz/iosStudyGroup.playground/Contents.swift
@@ -117,67 +117,207 @@ import Foundation
 //print("\(country.name)'s capital city is called \(country.capitalCity.name)")
 
 
-struct Resolution {
-    var width = 0
-    var height = 0
+//struct Resolution {
+//    var width = 0
+//    var height = 0
+//}
+//
+//class VideoMode {
+//    var resolution = Resolution()
+//    var interlaced = false
+//    var frameRate = 0.0
+//    var name: String?
+//}
+//
+//let someResolution = Resolution()
+//let someVideoMode = VideoMode()
+//
+//print("The width of someResolution is \(someResolution.width)")
+//print("The width of someVideoMode is \(someVideoMode.resolution.width)")
+//
+//someVideoMode.resolution.width = 1280
+//print("The width of someVideoMode is \(someVideoMode.resolution.width)")
+//
+//let hd = Resolution(width: 1920, height: 1080)
+//var cinema = hd
+//
+//cinema.width = 2048
+//print("cinema is now \(cinema.width) pixels wide")
+//print("hd is still \(hd.width) pixels wide")
+//
+//
+//
+//enum CompassPoint {
+//    case north, south, east, west
+//    mutating func turnNorth() {
+//        self = .north
+//    }
+//}
+//
+//var currentDirection = CompassPoint.west
+//let rememberedDirection = currentDirection
+//currentDirection.turnNorth()
+//
+//print("The current direction is \(currentDirection)")
+//print("The remembered direction is \(rememberedDirection)")
+//
+//
+//
+//let tenEighty = VideoMode()
+//tenEighty.resolution = hd
+//tenEighty.interlaced = true
+//tenEighty.name = "1080i"
+//tenEighty.frameRate = 25.0
+//
+//let alsoTenEighty = tenEighty
+//alsoTenEighty.frameRate = 30.0
+//
+//
+//print("The frameRate property of tenEighty is now \(tenEighty.frameRate)")
+//
+//
+//if tenEighty === alsoTenEighty {
+//    print("tenEighty and alsoTenEighty refer to the same Video instance.")
+//}
+
+
+//func greet(person: String) -> String {
+//    let greeting = "Hello, \(person)!"
+//    return greeting
+//}
+//
+//print(greet(person: "Anna"))
+//print(greet(person: "Brian"))
+//
+//func sayHelloWorld() -> String {
+//    return "Hello, World!"
+//}
+//print(sayHelloWorld())
+//
+//func greet(person: String, alreadyGreeted: Bool) -> String {
+//    if alreadyGreeted {
+//        return greetAgain(person: person)
+//    } else {
+//        return greet(person: person)
+//    }
+//}
+
+//func greet(person: String) {
+//    print("Hello, \(person)!")
+//}
+//greet(person: "Dave")
+
+//func printAndCount(string: String) -> Int {
+//    print(string)
+//    return string.count
+//}
+//
+//func printWithoutCounting(string: String) {
+//    let _ = printAndCount(string: string)
+//}
+//printAndCount(string: "Hello, World!")
+//printWithoutCounting(string: "hello, world!")
+
+//func minMax(array: [Int]) -> (min: Int, max: Int)? {
+//    if array.isEmpty { return nil }
+//    var currenMin = array[0]
+//    var currenMax = array[0]
+//    for value in array[1..<array.count] {
+//        if value < currenMin {
+//            currenMin = value
+//        } else if value > currenMax {
+//            currenMax = value
+//        }
+//    }
+//    return (currenMin, currenMax)
+//}
+
+//func greeting(for person: String) -> String {
+//    "Hello, \(person)!"
+//}
+//print(greeting(for: "Dave"))
+//
+//func anotherGreeting(for person: String) -> String {
+//    return "Hello, \(person)!"
+//}
+//print(anotherGreeting(for: "Dave"))
+
+//func someFunction(_ firstParameterName: Int, secondParameterNAme: Int) {
+//
+//}
+//someFunction(1, secondParameterNAme: 2)
+
+//func greet(person: String, from hometown: String) -> String {
+//    return "Hello \(person)! Glad you could visit from \(hometown)."
+//}
+//print(greet(person: "Bill", from: "Cupertino"))
+//
+//func someFunction(parameterWithoutDefault: Int, parameterWithDefault: Int = 12) {
+//
+//}
+//
+//someFunction(parameterWithoutDefault: 3) // parameterWithDefault: 12
+//someFunction(parameterWithoutDefault: 4, parameterWithDefault: 7)
+
+//func arithmeticMean(_ numbers: Double...) -> Double {
+//    var total: Double = 0
+//    for number in numbers {
+//        total += number
+//    }
+//    return total / Double(numbers.count)
+//}
+//
+//arithmeticMean(1, 2, 3, 4, 5)
+//arithmeticMean(3, 8.25, 18.75)
+
+//func swapTwoInts(_ a: inout Int, _ b: inout Int) {
+//    let temporaryA = a
+//    a = b
+//    b = temporaryA
+//}
+//
+//var someInt = 3
+//var anotherInt = 107
+//swap(&someInt, &anotherInt)
+//print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
+
+//func addTwoInts(_ a: Int, _ b: Int) -> Int {
+//    return a + b
+//}
+//
+//func multiplyTwoInts(_ a: Int, _ b: Int) -> Int {
+//    return a * b
+//}
+//
+//
+//var mathFunction: (Int, Int) -> Int = addTwoInts
+//print("Result: \(mathFunction(2,3))")
+//
+//mathFunction = multiplyTwoInts
+//print("Result: \(mathFunction(2,3))")
+//
+//func printMathResult(_ mathFunction: (Int, Int) -> Int, _ a: Int, _ b: Int) {
+//    print("Result: \(mathFunction(a, b))")
+//}
+//
+//printMathResult(addTwoInts, 3, 5)
+
+
+
+func chooseStepFunction(backward: Bool) -> (Int) -> Int {
+    func stepForward(_ input: Int) -> Int { return input + 1 }
+    func stepBackward(_ input: Int) -> Int { return input - 1 }
+    
+    return backward ? stepBackward : stepForward
 }
 
-class VideoMode {
-    var resolution = Resolution()
-    var interlaced = false
-    var frameRate = 0.0
-    var name: String?
+var currentValue = -4
+let moveNearerToZero = chooseStepFunction(backward: currentValue > 0)
+
+print("Counting to zero:")
+
+while currentValue != 0 {
+    print("\(currentValue)...")
+    currentValue = moveNearerToZero(currentValue)
 }
-
-let someResolution = Resolution()
-let someVideoMode = VideoMode()
-
-print("The width of someResolution is \(someResolution.width)")
-print("The width of someVideoMode is \(someVideoMode.resolution.width)")
-
-someVideoMode.resolution.width = 1280
-print("The width of someVideoMode is \(someVideoMode.resolution.width)")
-
-let hd = Resolution(width: 1920, height: 1080)
-var cinema = hd
-
-cinema.width = 2048
-print("cinema is now \(cinema.width) pixels wide")
-print("hd is still \(hd.width) pixels wide")
-
-
-
-enum CompassPoint {
-    case north, south, east, west
-    mutating func turnNorth() {
-        self = .north
-    }
-}
-
-var currentDirection = CompassPoint.west
-let rememberedDirection = currentDirection
-currentDirection.turnNorth()
-
-print("The current direction is \(currentDirection)")
-print("The remembered direction is \(rememberedDirection)")
-
-
-
-let tenEighty = VideoMode()
-tenEighty.resolution = hd
-tenEighty.interlaced = true
-tenEighty.name = "1080i"
-tenEighty.frameRate = 25.0
-
-let alsoTenEighty = tenEighty
-alsoTenEighty.frameRate = 30.0
-
-
-print("The frameRate property of tenEighty is now \(tenEighty.frameRate)")
-
-
-if tenEighty === alsoTenEighty {
-    print("tenEighty and alsoTenEighty refer to the same Video instance.")
-}
-
-
+print("zero!")
