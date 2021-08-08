226aa79e148d95475e3021e2779b40f68943e1ae
frozenxnow <frozenxnow@naver.com>
2021-08-08 20:54:28 +0900

    2021-08-08

--- swift/StudyQuiz/iosStudyGroup.playground/Contents.swift
+++ swift/StudyQuiz/iosStudyGroup.playground/Contents.swift
@@ -304,20 +304,73 @@ import Foundation
 
 
 
-func chooseStepFunction(backward: Bool) -> (Int) -> Int {
-    func stepForward(_ input: Int) -> Int { return input + 1 }
-    func stepBackward(_ input: Int) -> Int { return input - 1 }
-    
-    return backward ? stepBackward : stepForward
+//func chooseStepFunction(backward: Bool) -> (Int) -> Int {
+//    func stepForward(_ input: Int) -> Int { return input + 1 }
+//    func stepBackward(_ input: Int) -> Int { return input - 1 }
+//    
+//    return backward ? stepBackward : stepForward
+//}
+//
+//var currentValue = -4
+//let moveNearerToZero = chooseStepFunction(backward: currentValue > 0)
+//
+//print("Counting to zero:")
+//
+//while currentValue != 0 {
+//    print("\(currentValue)...")
+//    currentValue = moveNearerToZero(currentValue)
+//}
+//print("zero!")
+
+
+
+struct TimesTable {
+    let multiplier: Int
+    subscript(index: Int) -> Int {
+        return multiplier * index
+    }
 }
 
-var currentValue = -4
-let moveNearerToZero = chooseStepFunction(backward: currentValue > 0)
 
-print("Counting to zero:")
+let threeTimesTable = TimesTable(multiplier: 3)
+print("six times three is \(threeTimesTable[6])")
 
-while currentValue != 0 {
-    print("\(currentValue)...")
-    currentValue = moveNearerToZero(currentValue)
+var numberOfLegs = ["Spider": 8, "ant": 6, "cat": 4]
+numberOfLegs["bird"] = 2
+
+struct Matrix {
+    let rows: Int, columns: Int
+    var grid: [Double]
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        grid = Array(repeating: 0.0, count: rows * columns)
+    }
+    func indexIsValid(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            grid[(row * columns) + column] = newValue
+        }
+    }
 }
-print("zero!")
+
+var testMatrix = Matrix(rows: 2, columns: 2)
+testMatrix[0, 1] = 1.5
+testMatrix[1, 0] = 3.2
+
+enum Planet: Int {
+    case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
+    static subscript(n: Int) -> Planet {
+        return Planet(rawValue: n)!
+    }
+}
+
+let mars = Planet[4]
+print(mars)
